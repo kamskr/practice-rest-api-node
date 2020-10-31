396681acb73b37731e8130f053c14a2e0868672c
Kamil <sikorakamil26@gmail.com>
2020-10-31 16:04:11 +0100

    connection to database

--- lib/config/app.ts
+++ lib/config/app.ts
@@ -1,21 +1,34 @@
 import * as express from "express"
 import * as bodyParser from "body-parser"
+import * as mongoose from 'mongoose'
+import environment from "../environment";
 import { TestRoutes } from "../routes/test_routes";
 import { CommonRoutes } from "../routes/common_routes";
 
 class App {
     public app: express.Application;
+    // public mongoUrl: string = 'mongodb://localhost/' + environment.getDBName();
+    public mongoUrl: string = "mongodb://admin:secure@localhost:27017/admin"
     private test_routes: TestRoutes = new TestRoutes();
     private common_routes: CommonRoutes = new CommonRoutes();
     constructor() {
         this.app = express();
         this.config();
-        this.test_routes.route(this.app)
-        this.common_routes.route(this.app)
-    }
-    private config(): void {
+        this.mongoSetup();
+        this.test_routes.route(this.app);
+        this.common_routes.route(this.app);
+     }z
+  
+     private config(): void {
+        // support application/json type post data
         this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({extended: false}));
-    }
+        //support application/x-www-form-urlencoded post data
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+     }
+  
+     private mongoSetup(): void {
+        mongoose.connect(this.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
+     }
+  
 }
 export default new App().app;
