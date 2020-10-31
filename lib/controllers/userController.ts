b3e3e79e998e26a3b3c10b03c7e0abee194d144b
Kamil <sikorakamil26@gmail.com>
2020-10-31 16:13:43 +0100

    added models

---
+++ lib/controllers/userController.ts
@@ -0,0 +1,7 @@
+
+import { Request, Response } from 'express';
+import { insufficientParameters, mongoError, successResponse, failureResponse } from '../modules/common/service';
+import { IUser } from '../modules/users/model';
+import UserService from '../modules/users/service';
+import e = require('express');
+
